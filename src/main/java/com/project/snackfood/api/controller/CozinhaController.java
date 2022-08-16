dec2587239dec828d503585f08f56314fe06e184
jonas <jonas.peixoto.087@gmail.com>
2022-08-16 19:40:54 -0300

    adicionando rotas requisiçao post

--- src/main/java/com/project/snackfood/api/controller/CozinhaController.java
+++ src/main/java/com/project/snackfood/api/controller/CozinhaController.java
@@ -3,9 +3,14 @@ package com.project.snackfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.snackfood.domain.model.Cozinha;
@@ -24,9 +29,21 @@ public class CozinhaController {
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
 	}
+
 	
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		if (cozinha !=null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		return ResponseEntity.notFound().build(); // nao retorna corpo, mais retorna o erro 404
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+		return cozinhaRepository.salvar(cozinha);
 	}
 }
--- src/main/java/com/project/snackfood/domain/model/Cozinha.java
+++ src/main/java/com/project/snackfood/domain/model/Cozinha.java
@@ -6,8 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -21,7 +19,6 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // gerar id altomaticamente
 	private Long id;
 	
-	@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
 
